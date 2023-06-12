82eadd477422ce77036ff0b82bcfa6efd6f5928d
svensachtleber <svensachtleber2@gmail.com>
2023-06-12 14:45:17 +0200

    fixed two html pages problem

--- DataServer_Moritz.py
+++ DataServer_Moritz.py
@@ -7,17 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        # Hier können Sie die POST-Daten verarbeiten
-        username = request.form.get('username')
-        password = request.form.get('password')
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if request.method == 'POST':
+#         # Hier können Sie die POST-Daten verarbeiten
+#         username = request.form.get('username')
+#         password = request.form.get('password')
+#
+#
+#         return "Danke für Ihre Anmeldung!"
 
-
-        return "Danke für Ihre Anmeldung!"
-
-a = 5
 
 if __name__ == "__main__":
-    app.run(host="10.130.240.91", port=80, debug=True)
+    app.run(host="localhost", port=80, debug=True)
+
