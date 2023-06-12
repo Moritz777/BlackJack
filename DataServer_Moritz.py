fe13e16331001cd9036e85531054e637a3930a6c
Moritz <moritz-heinke@web.de>
2023-06-12 14:32:16 +0200

    Lesen aus Web

--- DataServer_Moritz.py
+++ DataServer_Moritz.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,6 +7,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Hier können Sie die POST-Daten verarbeiten
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+
+        return "Danke für Ihre Anmeldung!"
+
+a = 5
 
 if __name__ == "__main__":
-    app.run(host="10.130.240.91", debug=True)
+    app.run(host="10.130.240.91", port=80, debug=True)
