880fd5da4bdae37358799c326fb6b8f043491094
Erik <erik.vormann@gmail.com>
2023-06-13 11:34:58 +0200

    Erstellung create_new_user() und insert_into_db()

--- Tools.py
+++ Tools.py
@@ -1,6 +1,10 @@
 import hashlib
 
 def hash_password(password):
+    """
+    Verschlüsselt das Passwort mit dem SHA-256 Algorithmus.
+    :return: Gehashtes Passwort als 64-stellige Zeichenkette.
+    """
     sha256 = hashlib.sha256()
     sha256.update(password.encode('utf-8'))
     hashed_password = sha256.hexdigest()
