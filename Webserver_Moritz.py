e5a723701810d83fb31d5b141ad712fa831439f3
Erik <erik.vormann@gmail.com>
2023-06-13 12:24:25 +0200

    weiß auch nicht mehr was ich gemacht hab

--- Webserver_Moritz.py
+++ Webserver_Moritz.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import db_connection
 from Tools import hash_password
+from user_class import User
 
 # from create_new_user_on_registry import new_user
 
@@ -32,7 +33,7 @@ def registrierung():
     if request.method == 'POST':
         username = request.form.get('username')
         hashed_password = hash_password(request.form.get('password'))
-        # new_user.create_new_user(username, password)
+        new_user = User(username, hashed_password)
         if db_connection.check_login(username,hashed_password):
             error_message = "Benutzernamen bereits vergeben"  # Fehlermeldung
             return render_template('regestrierung.html', error_message=error_message)
--- user_class.py
+++ user_class.py
@@ -1,21 +1,13 @@
-import hashlib
-import pyodbc
+import db_connection
 
-server = 'provadis-it-ausbildung.de'
-database = 'BlackJack03'
-username = 'BlackJackUser03'
-password = 'ProvadisBlackJackUser03__'
-connection_string = 'DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + database + ';UID=' + username + ';PWD=' + password
 
 class User:
-    def __init__(self, userid, username, hash_password, capital):
-        self.userid = userid
+    def __init__(self, username, hash_password, capital):
+        self.userid = None
         self.username = username
         self.hash_password = hash_password
-        self.capital = capital
-        create_new_user()
+        self.capital = 5000
+        db_connection.create_new_user(username, hash_password, capital)
 
     def change_username(self, new_username):
         self.username = new_username
-
-    def create_new_user(self, username, hashed_password):
