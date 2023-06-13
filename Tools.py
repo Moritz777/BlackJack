d6c7e42f3abd95f2db63a3a4bd5b359ca57589f0
Erik <erik.vormann@gmail.com>
2023-06-13 11:36:07 +0200

    Kommentar zur Hashfunktion

--- Tools.py
+++ Tools.py
@@ -3,7 +3,7 @@ import hashlib
 def hash_password(password):
     """
     Verschlüsselt das Passwort mit dem SHA-256 Algorithmus.
-    :return: Gehashtes Passwort als 64-stellige Zeichenkette.
+    :return: gehashtes Passwort als 64-stellige Zeichenkette
     """
     sha256 = hashlib.sha256()
     sha256.update(password.encode('utf-8'))
