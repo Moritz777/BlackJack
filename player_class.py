a8c9501c3e72ecddb2b81d2b45a0f4c91204c721
svensachtleber <svensachtleber2@gmail.com>
2023-06-14 08:45:47 +0200

    designing indexpage and creating player_class

---
+++ player_class.py
@@ -0,0 +1,7 @@
+
+
+class Player:
+    def __init__(self, username: str, credit: int, wants_to_play: bool):
+        self.username = username
+        self.credit = credit
+        self.wants_to_play = wants_to_play
