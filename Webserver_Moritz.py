4dc037280025f4477d9ccec82b65db73bec015d5
Moritz <moritz-heinke@web.de>
2023-06-14 10:04:27 +0200

    bild

--- Webserver_Moritz.py
+++ Webserver_Moritz.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template, request, redirect
 import db_connection
 from Tools import hash_password
+from player_class import Player
 from user_class import User
 
 # from create_new_user_on_registry import new_user
 
 # import create_new_user_on_registry
 
-
+players = []
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -18,6 +19,9 @@ def index():
         hashed_password = hash_password(request.form.get('password'))
 
         if db_connection.check_login(username,hashed_password):
+            player = Player(username)
+            players.append(player)
+            print(players)
             return render_template('startPage.html', username=username)
         else:
             error_message = "Benutzer nicht vorhanden"  # Fehlermeldung
@@ -40,8 +44,7 @@ def registrierung():
         else:
             new_user = User(username, hashed_password)
             return render_template('startPage.html', username="Benutzer wurde angelegt")
-    else:
-        return render_template('regestrierung.html')
+    return render_template('regestrierung.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='81', debug=True)
