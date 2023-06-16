db6f761b3afe944134084d1748e6b72d85b611eb
svensachtleber <svensachtleber2@gmail.com>
2023-06-16 13:11:55 +0200

    session id logik erfolgreich

--- Webserver_Moritz.py
+++ Webserver_Moritz.py
@@ -1,10 +1,9 @@
+import random
 from datetime import date, datetime
 from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from flask_socketio import SocketIO, emit
-
 from flask import Flask, render_template, request, redirect, url_for, flash
 import db_connection
-import game_logic
 from Tools import hash_password
 from player_class import Player
 from user_class import User
@@ -16,7 +15,9 @@ control = control()
 player = None
 app.secret_key = 'sventegetscookie'
 socketio = SocketIO(app)
+lobbies = {}
 online_users = []
+
 
 app.secret_key = 'your_secret_key'  # Set a secret key for flashing messages
 
@@ -69,18 +70,25 @@ def registrierung():
 
 @app.route('/startPage', methods=['GET', 'POST'])
 def random_session():
+
+    username = session['username']
+
     if request.method == 'POST':
 
         if request.form['btn'] == 'Zufälligem Spiel beitreten':
             return redirect('/game_template')
 
         if request.form['btn'] == 'Spiel hosten':
-            return redirect('/display')
+            game_session_id = username + "_" + str(random.randint(100000,999999))
+            session['game_session'] = game_session_id
+            lobbies[game_session_id] = {'players': []}
+            print(lobbies)
+
+            return redirect('/users')
 
         if request.form['btn'] == 'Spiel beitreten':
             return redirect('/lobby_list')
 
-    username = session['username']
     return render_template('startPage.html', username=username)
 
 
@@ -107,36 +115,45 @@ def get_data():
 @app.route('/display', methods=['POST', 'GET'])
 def display():
 
+    game_session_id = session['game_session']
+
     if request.method == 'POST':
         return redirect('/users')
 
     username = session.get('username')
-    return render_template('display.html', username=username)
+    return render_template('display.html', username=username, game_session_id=game_session_id)
 
 
 @app.route('/users', methods=['POST', 'GET'])
 def users():
+    username = session.get('username')
+    game_session = session['game_session']
 
     if request.method == 'POST':
-        return redirect('/display')
+        lobbies.pop(game_session)
+        print(lobbies)
+        return redirect('/startPage')
 
-    username = session.get('username')
-    return render_template('users.html', username=username)
+    return render_template('users.html', username=username, game_session=game_session)
 
 
 @socketio.on('connect')
 def handle_connect():
     username = session.get('username')
-    online_users.append(username)
-    emit('user_update', online_users, broadcast=True)
+    game_session = session['game_session']
+    lobbies[game_session]['players'].append(username)
+    print(lobbies)
+    emit('user_update', lobbies[game_session]['players'], broadcast=True)
 
 
 @socketio.on('disconnect')
 def handle_disconnect():
     username = session.get('username')
-    if username in online_users:
-        online_users.remove(username)
-    emit('user_update', online_users, broadcast=True)
+    game_session = session['game_session']
+    if username in lobbies[game_session]['players']:
+        lobbies[game_session]['players'].remove(username)
+    print(lobbies)
+    emit('user_update', lobbies[game_session]['players'], broadcast=True)
 
 
 # ------------------------
