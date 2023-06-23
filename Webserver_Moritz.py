d46a55d3a2e4155b1a3560c7ae33f725dbb3f2a0
Janine <lalapo69420@gmail.com>
2023-06-23 08:41:11 +0200

    emit for startButton

--- Webserver_Moritz.py
+++ Webserver_Moritz.py
@@ -216,11 +216,11 @@ def handle_disconnect():
     print(users_dict["open_lobbies"])
     emit('user_update', users_list, room=host_name)
 
-@socketio.on('sayHiToEveryone')
-def say_hi_to_everyone():
+@socketio.on('startGameForEveryone')
+def emit_game_start():
     print("hi to everyone")
-    phrase = "Hi to everyone!"
-    emit('getHiFromPython', phrase, broadcast=True)
+    fiktive_karten = ["Herz", 4] # Hier könnten Karten übergeben werden
+    emit('startSuccessFromPython', fiktive_karten, broadcast=True)
 
 if __name__ == "__main__":
     socketio.run(app, allow_unsafe_werkzeug="True", host="0.0.0.0", port="81", debug="True")
